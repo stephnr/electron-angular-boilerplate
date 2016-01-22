f1c3fe14b81b03185dc118f2c420c133b52c550f
Stephen Rodriguez <steprodriguez10@gmail.com>
2016-01-21 19:08:33 -0500

    shebang for bash

--- scripts/load_globals.sh
+++ scripts/load_globals.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 DEPENDENCIES=( "electron-prebuilt:electron" "electron-packager:electron-packager" "node-inspector:node-inspector" "bower:bower" "jspm:jspm" "gulp:gulp" "jest-cli:jest" )
 for DEP in ${DEPENDENCIES[@]}; do
   PKG="${DEP%%:*}"
